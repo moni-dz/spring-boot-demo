cfc28a67da8d066506a2b597c4aea7cbc843380c
Lythe Marvin Lacre <lythe1107@gmail.com>
2026-08-12 09:59:06 +0800

    chore(test): add MySQL test infrastructure

---
+++ src/test/kotlin/in/over/demo/MySqlTestConfiguration.kt
@@ -0,0 +1,13 @@
+package `in`.over.demo
+
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection
+import org.springframework.context.annotation.Bean
+import org.testcontainers.mysql.MySQLContainer
+
+@TestConfiguration(proxyBeanMethods = false)
+class MySqlTestConfiguration {
+    @Bean
+    @ServiceConnection
+    fun mysqlContainer() = MySQLContainer("mysql:9.7.1")
+}
--- src/test/kotlin/in/over/demo/TimeRecordApplicationTests.kt
+++ src/test/kotlin/in/over/demo/TimeRecordApplicationTests.kt
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.Import
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete
@@ -24,6 +25,7 @@ import kotlin.test.assertTrue
 import kotlin.time.Clock
 
 @SpringBootTest
+@Import(MySqlTestConfiguration::class)
 class TimeRecordApplicationTests {
     @Autowired
     private lateinit var appContext: WebApplicationContext
